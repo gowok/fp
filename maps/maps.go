acca624427010f84c3460d2a0fbc7fdd2161ea08
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-01-27 00:18:35 +0700

    feat(maps-values): enabled

--- maps/maps.go
+++ maps/maps.go
@@ -2,8 +2,17 @@ package maps
 
 func Keys[T comparable, U any](input map[T]U) []T {
 	result := []T{}
-	for k, _ := range input {
+	for k := range input {
 		result = append(result, k)
+	}
+
+	return result
+}
+
+func Values[T comparable, U any](input map[T]U) []U {
+	result := []U{}
+	for _, v := range input {
+		result = append(result, v)
 	}
 
 	return result
