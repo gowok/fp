86379284000d4f7cef8bfc3c8f00696c4b879369
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-11 12:30:06 +0700

    add test for slices

---
+++ slices/slices_test.go
@@ -0,0 +1,18 @@
+package slices
+
+import (
+	"testing"
+
+	"github.com/golang-must/must"
+)
+
+func Test_Foreach(t *testing.T) {
+	t.Run("positive", func(t *testing.T) {
+		sample := []int{1, 2, 3}
+		must := must.New(t)
+
+		ForEach(sample, func(s, i int) {
+			must.Equal(sample[i], s)
+		})
+	})
+}
