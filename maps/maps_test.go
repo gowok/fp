3069fdff0a84f4ffdb78838b41aa7ab528b3920d
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-11 12:53:55 +0700

    test(maps-keys): ✅ pass

---
+++ maps/maps_test.go
@@ -0,0 +1,23 @@
+package maps
+
+import (
+	"testing"
+
+	"github.com/golang-must/must"
+)
+
+var sample = map[string]int{
+	"x": 1,
+	"y": 2,
+	"z": 3,
+}
+var keys = []string{"x", "y", "z"}
+var values = []int{1, 2, 3}
+
+func Test_Keys(t *testing.T) {
+	actual := Keys(sample)
+
+	must := must.New(t)
+	must.Equal(len(keys), len(actual))
+}
+
