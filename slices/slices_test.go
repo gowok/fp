d00e8d17589463b22906f97196973c395a669f69
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-11 12:34:32 +0700

    test(slices-filter): ✅ pass

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -7,23 +7,30 @@ import (
 )
 
 func Test_Foreach(t *testing.T) {
-	t.Run("positive", func(t *testing.T) {
-		sample := []int{1, 2, 3}
-		must := must.New(t)
+	sample := []int{1, 2, 3}
+	must := must.New(t)
 
-		ForEach(sample, func(s, i int) {
-			must.Equal(sample[i], s)
-		})
+	ForEach(sample, func(s, i int) {
+		must.Equal(sample[i], s)
 	})
 }
 
 func Test_GoForeach(t *testing.T) {
-	t.Run("positive", func(t *testing.T) {
-		sample := []int{1, 2, 3}
-		must := must.New(t)
+	sample := []int{1, 2, 3}
+	must := must.New(t)
 
-		GoForEach(sample, func(s, i int) {
-			must.Equal(sample[i], s)
-		})
+	GoForEach(sample, func(s, i int) {
+		must.Equal(sample[i], s)
 	})
+}
+
+func Test_Filter(t *testing.T) {
+	expected := []int{1}
+	sample := []int{1, 2, 3}
+	actual := Filter(sample, func(s, i int) bool {
+		return s == 1
+	})
+
+	must := must.New(t)
+	must.Equal(expected, actual)
 }
